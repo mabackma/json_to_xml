e9c820ed3fd219dfbd3c1d8ca414b7f7fbc02233
Marc Bäckman <marc.backman@avoin.systems>
2025-11-13 05:04:49 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,9 @@ use json_to_xml::generate_xml::json_to_xml;
 use std::fs;
 
 fn main() {
-    println!("Hello, world!");
-
-    let file_json = fs::read_to_string("file_forestpropertydata2.json").expect("Could not read the JSON file");
-    let file_json_value: serde_json::Value = serde_json::from_str(&file_json).unwrap();
-    let file_new_xml = json_to_xml(&file_json_value, "People");
+    let json_string = fs::read_to_string("file_forestpropertydata2.json").expect("Could not read the JSON file");
+    let json_value: serde_json::Value = serde_json::from_str(&json_string).unwrap();
+    let file_new_xml = json_to_xml(&json_value, "People");
 
     // Save the new XML content to files
     std::fs::write("file_back_to_xml.xml", &file_new_xml).expect("Unable to write data");
