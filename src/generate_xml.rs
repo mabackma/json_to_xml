5e96d4b6d1d04a3a18a60516a7b751d0d15075f2
Marc Bäckman <marc.backman@avoin.systems>
2025-11-14 03:22:30 +0200

    refactor

--- src/generate_xml.rs
+++ src/generate_xml.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use quick_xml::Writer;
 use quick_xml::events::{BytesEnd, BytesStart, BytesText, BytesDecl, Event};
-use serde_json::{Value, from_str};
+use serde_json::{Value, Number, Map, from_str};
 use std::collections::HashMap;
 use std::io::Cursor;
 
@@ -133,130 +133,22 @@ fn create_xml_element(
 
         // Handle objects
         Value::Object(map) => {
-            let mut parent_tag = parent_tag.to_string();
-            parent_tag = capitalize_word(&parent_tag);
-
-            let mut element = BytesStart::new(parent_tag);
-
-            // Extract attributes
-            let attributes: HashMap<_, _> = map
-                .iter()
-                .filter(|(key, _)| key.starts_with("@"))
-                .map(|(key, value)| (&key[1..], value))
-                .collect();
-
-            // Add attributes to the element
-            for (key, value) in &attributes {
-                if let Some(value_str) = value.as_str() {
-                    element.push_attribute((*key, value_str));
-                }
-            }
-
-            // Write start tag with attributes, if any
-            if !attributes.is_empty() {
-                writer
-                    .write_event(Event::Start(element.to_owned()))
-                    .expect("Unable to write start tag");
-            }
-
-            if map.contains_key("$text") {
-                let text_content = map.get("$text").unwrap().as_str().unwrap();
-                writer
-                    .write_event(Event::Text(BytesText::new(&text_content)))
-                    .expect("Unable to write text");
-            }
-
-            // Process key-value pairs
-            for (key, value) in map {
-                // Reset the element for the next iteration				  
-                let mut key_tag = key.to_string();
-                key_tag = capitalize_word(&key_tag);
-
-                element = BytesStart::new(key_tag.clone());
-
-                // Write self-closing tag if the object is empty
-                if value.is_object() && value.as_object().unwrap().is_empty() {
-                    writer
-                        .write_event(Event::Empty(element.to_owned()))
-                        .expect("Unable to write self-closing tag");
-
-                    continue;
-                }
-
-                // Skip attributes
-                if key.starts_with("@") || key == "$text" {
-                    continue;
-                } else {
-
-                    // Write the start tag if the value is not an attribute or an array with a first key as an attribute
-                    if !(is_attribute_key(value) || is_array_with_attribute_key(value)) {
-                        writer
-                            .write_event(Event::Start(element.to_owned()))
-                            .expect("Unable to write start tag");
-                    }
-
-					// Recursively process nested elements
-					create_xml_element(value, writer, key);
-					
-                    // Write the closing tag if the value is not an array
-                    if !value.is_array() {
-                        writer
-                            .write_event(Event::End(BytesEnd::new(key_tag)))
-                            .expect("Unable to write end tag");
-                    }
-                }
-            }
+            handle_object(writer, map, parent_tag);
         },
 
         // Handle arrays by processing each item inside the array
         Value::Array(arr) => {
-            let mut parent_tag = parent_tag.to_string();
-            parent_tag = capitalize_word(&parent_tag);
-            
-            for (i, value) in arr.iter().enumerate() {
-
-                // Get the first key of the object 
-                if value.is_object() {
-                    let first_key = value.as_object().unwrap().keys().next().unwrap();
-
-                    // Write the start tag for all non-attribute elements, skipping the first one
-                    if !first_key.starts_with("@") && i > 0 {
-                        writer
-                            .write_event(Event::Start(BytesStart::new(&parent_tag)))
-                            .expect("Unable to write start tag"); 
-                    } 
-                }
-
-                // Process each element of the array as a separate XML tag
-                create_xml_element(value, writer, &parent_tag);
-
-                // Write the closing tag
-                writer
-                    .write_event(Event::End(BytesEnd::new(&parent_tag)))
-                    .expect("Unable to write end tag");
-            }
+            handle_array(writer, arr, parent_tag);
         },
 
         // Handle strings as text content
         Value::String(s) => {
-            writer
-                .write_event(Event::Text(BytesText::new(s)))
-                .expect("Unable to write text");
+            handle_string(writer, s);
         },
 
         // Handle number as text content
         Value::Number(num) => {
-            let num_str = if num.is_i64() {
-                num.as_i64().unwrap().to_string()
-            } else if num.is_f64() {
-                format!("{}", num.as_f64().unwrap()) // Convert float properly
-            } else {
-                String::new()
-            };
-
-            writer
-                .write_event(Event::Text(BytesText::new(&num_str)))
-                .expect("Unable to write number");
+            handle_number(writer, num);
         }
 
         // Skip unsupported types
@@ -298,5 +190,129 @@ fn capitalize_word(word: &str) -> String {
     match chars.next() {
         None => String::new(),
         Some(f) => f.to_uppercase().collect::<String>() + chars.as_str(),
+    }
+}
+
+fn handle_array(writer: &mut Writer<Cursor<Vec<u8>>>, arr: &Vec<Value>, parent_tag: &str) {
+    let mut parent_tag = parent_tag.to_string();
+    parent_tag = capitalize_word(&parent_tag);
+    
+    for (i, value) in arr.iter().enumerate() {
+
+        // Get the first key of the object 
+        if value.is_object() {
+            let first_key = value.as_object().unwrap().keys().next().unwrap();
+
+            // Write the start tag for all non-attribute elements, skipping the first one
+            if !first_key.starts_with("@") && i > 0 {
+                writer
+                    .write_event(Event::Start(BytesStart::new(&parent_tag)))
+                    .expect("Unable to write start tag"); 
+            } 
+        }
+
+        // Process each element of the array as a separate XML tag
+        create_xml_element(value, writer, &parent_tag);
+
+        // Write the closing tag
+        writer
+            .write_event(Event::End(BytesEnd::new(&parent_tag)))
+            .expect("Unable to write end tag");
+    }
+}
+
+fn handle_string(writer: &mut Writer<Cursor<Vec<u8>>>, s: &String) {
+    writer
+        .write_event(Event::Text(BytesText::new(s)))
+        .expect("Unable to write text");
+}
+
+fn handle_number(writer: &mut Writer<Cursor<Vec<u8>>>, num: &Number) {
+    let num_str = if num.is_i64() {
+        format!("{}", num.as_i64().unwrap())
+    } else if num.is_f64() {
+        format!("{}", num.as_f64().unwrap())
+    } else {
+        String::new()
+    };
+
+    writer
+        .write_event(Event::Text(BytesText::new(&num_str)))
+        .expect("Unable to write number");
+}
+
+fn handle_object(writer: &mut Writer<Cursor<Vec<u8>>>, map: &Map<String, Value>, parent_tag: &str) {
+    let mut parent_tag = parent_tag.to_string();
+    parent_tag = capitalize_word(&parent_tag);
+
+    let mut element = BytesStart::new(parent_tag);
+
+    // Extract attributes
+    let attributes: HashMap<_, _> = map
+        .iter()
+        .filter(|(key, _)| key.starts_with("@"))
+        .map(|(key, value)| (&key[1..], value))
+        .collect();
+
+    // Add attributes to the element
+    for (key, value) in &attributes {
+        if let Some(value_str) = value.as_str() {
+            element.push_attribute((*key, value_str));
+        }
+    }
+
+    // Write start tag with attributes, if any
+    if !attributes.is_empty() {
+        writer
+            .write_event(Event::Start(element.to_owned()))
+            .expect("Unable to write start tag");
+    }
+
+    if map.contains_key("$text") {
+        let text_content = map.get("$text").unwrap().as_str().unwrap();
+        writer
+            .write_event(Event::Text(BytesText::new(&text_content)))
+            .expect("Unable to write text");
+    }
+
+    // Process key-value pairs
+    for (key, value) in map {
+        // Reset the element for the next iteration				  
+        let mut key_tag = key.to_string();
+        key_tag = capitalize_word(&key_tag);
+
+        element = BytesStart::new(key_tag.clone());
+
+        // Write self-closing tag if the object is empty
+        if value.is_object() && value.as_object().unwrap().is_empty() {
+            writer
+                .write_event(Event::Empty(element.to_owned()))
+                .expect("Unable to write self-closing tag");
+
+            continue;
+        }
+
+        // Skip attributes
+        if key.starts_with("@") || key == "$text" {
+            continue;
+        } else {
+
+            // Write the start tag if the value is not an attribute or an array with a first key as an attribute
+            if !(is_attribute_key(value) || is_array_with_attribute_key(value)) {
+                writer
+                    .write_event(Event::Start(element.to_owned()))
+                    .expect("Unable to write start tag");
+            }
+
+            // Recursively process nested elements
+            create_xml_element(value, writer, key);
+            
+            // Write the closing tag if the value is not an array
+            if !value.is_array() {
+                writer
+                    .write_event(Event::End(BytesEnd::new(key_tag)))
+                    .expect("Unable to write end tag");
+            }
+        }
     }
 }
